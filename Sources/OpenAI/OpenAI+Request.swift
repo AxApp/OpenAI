91fad843779fd98f3a9365ab52986f0a192ffea2
linhey <is.linhey@outlook.com>
2023-06-26 19:11:58 +0800

    chore: 调整

--- Sources/OpenAI/OpenAI+Request.swift
+++ Sources/OpenAI/OpenAI+Request.swift
@@ -12,20 +12,27 @@ import OpenAICore
 
 public extension OpenAI {
     
-    struct ChatStreamItem {
-        public let chat: OAIChat.Chat
-        public let stream: DataStreamRequest.Stream<Data, Never>?
-    }
-    
     func chats(_ query: OAIChat.Query) async throws -> OAIChat.Response {
         var query = query
         query.stream = false
         return try await post(OAIChat(query))
     }
     
-    func chats(stream query: OAIChat.Query, interval: TimeInterval? = nil) -> AsyncThrowingStream<ChatStreamItem, Error> {
-        if let stream = query.stream, stream {
-            return .init { continuation in
+    func chats(_ builder: (_ query: inout OAIChat.Query) -> Void) async throws -> OAIChat.Response {
+        var query = OAIChat.Query()
+        builder(&query)
+        return try await chats(query)
+    }
+    
+    func chats(stream query: OAIChat.Query, interval: TimeInterval? = nil) async -> AsyncThrowingStream<OAIChat.Chat, Error> {
+        let (stream, continuation) = AsyncThrowingStream<OAIChat.Chat, Error>.makeStream()
+        do {
+            if query.stream == false {
+                if let message = try await self.chats(query).choices.first?.message {
+                    continuation.yield(message)
+                }
+                continuation.finish()
+            } else {
                 var date = Date()
                 try? chats(query: query) { result in
                     let now = Date()
@@ -39,22 +46,21 @@ public extension OpenAI {
                         continuation.finish(throwing: error)
                     case .success(let value):
                         if let chat = value.choices.first?.message {
-                            continuation.yield(.init(chat: chat, stream: nil))
+                            continuation.yield(chat)
                         }
                         continuation.finish()
                     }
                 }
             }
-        } else {
-            return .init {
-                let response = try await self.chats(query)
-                return .init(chat: response.choices.first?.message ?? .init(role: .assistant, content: ""), stream: nil)
-            }
+        } catch {
+            continuation.finish(throwing: error)
         }
+        
+        return stream
     }
     
-    func chats(query: OAIChat.Query) -> AnyPublisher<ChatStreamItem, Error> {
-        let suject = PassthroughSubject<ChatStreamItem, Error>()
+    func chats(query: OAIChat.Query) -> AnyPublisher<OAIChat.Chat, Error> {
+        let suject = PassthroughSubject<OAIChat.Chat, Error>()
         do {
             try chats(query: query) { result in
                 suject.send(result)
@@ -64,7 +70,7 @@ public extension OpenAI {
                     suject.send(completion: .failure(error))
                 case .success(let value):
                     if let chat = value.choices.first?.message {
-                        suject.send(.init(chat: chat, stream: nil))
+                        suject.send(chat)
                     }
                     suject.send(completion: .finished)
                 }
@@ -75,8 +81,49 @@ public extension OpenAI {
         return suject.eraseToAnyPublisher()
     }
     
+   static func merge(deltas: [OAIChat.DeltaChatResult]) -> OAIChat.Chat {
+         
+        var role: String = ""
+        var content: String?
+        var function_call: OAIChat.FunctionCall?
+        
+        let deltas = deltas.compactMap(\.choices.first?.delta)
+        for delta in deltas {
+            if let value = delta.role {
+                role = value
+            }
+            if let value = delta.content {
+                if content == nil {
+                    content = value
+                } else {
+                    content?.append(value)
+                }
+            }
+            
+            if let value = delta.function_call {
+                if function_call == nil {
+                    function_call = .init(name: value.name ?? "", arguments: value.arguments ?? "")
+                    continue
+                }
+                
+                if let value = value.name {
+                    function_call?.name = value
+                }
+                
+                if let value = value.arguments {
+                    function_call?.arguments.append(value)
+                }
+            }
+        }
+        
+        return .init(role: role,
+                     content: content ?? "",
+                     function_call: function_call)
+
+    }
+    
     func chats(query: OAIChat.Query,
-               stream: @escaping (_ stream: ChatStreamItem) -> Void,
+               stream: @escaping (_ stream: OAIChat.Chat) -> Void,
                completion: @escaping (Result<OAIChat.Response, Error>) -> Void) throws {
         guard let url = URL(string: uri(.chats)) else {
             completion(.failure(OAIError.invalidURL))
@@ -88,11 +135,10 @@ public extension OpenAI {
                                  headers: .init(headers().map({HTTPHeader(name: $0.key, value: $0.value)})))
         var query = query
         query.stream = true
-        request.httpBody = try JSONEncoder().encode(query)
+        request.httpBody = try query.serialize().rawData()
         
         let decoder = JSONDecoder()
         var blocks = [OAIChat.DeltaChatResult]()
-        var chat = OAIChat.Chat(role: .assistant, content: "")
         let parser = EventStreamParser()
         AF.streamRequest(request).responseStream { response in
             switch response.event {
@@ -106,8 +152,8 @@ public extension OpenAI {
                             .compactMap({ $0.data(using: .utf8) })
                             .compactMap({ try? decoder.decode(OAIChat.DeltaChatResult.self, from: $0) })
                         blocks.append(contentsOf: list)
-                        chat.content += list.compactMap(\.choices.first?.delta?.content).joined()
-                        stream(.init(chat: chat, stream: response))
+                        let result = OpenAI.merge(deltas: blocks)
+                        stream(result)
                     }
                 }
             case let .complete(result):
@@ -119,8 +165,8 @@ public extension OpenAI {
                                             created: first.created,
                                             model: first.model,
                                             choices: [.init(index: first.choices.first?.index ?? 0,
-                                                            message: chat,
-                                                            finish_reason: first.choices.last?.finish_reason ?? "")],
+                                                            message: OpenAI.merge(deltas: blocks),
+                                                            finish_reason: first.choices.last?.finish_reason ?? .none)],
                                             usage: .init(prompt_tokens: 0,
                                                          completion_tokens: 0,
                                                          total_tokens: 0))
@@ -138,7 +184,7 @@ public extension OpenAI {
         let serialize = AF.request(uri(api.path),
                                        method: .post,
                                        headers: .init(.init(headers())), requestModifier: { request in
-            request.httpBody = try api.query.serializeData()
+            request.httpBody = try api.query.serialize().rawData()
         }).serializingData()
         
         let response = await serialize.response
@@ -155,7 +201,7 @@ public extension OpenAI {
     
     func get<API: OAIAPI>(_ api: API) async throws -> API.Response {
         let serialize = try AF.request(uri(api.path),
-                                    parameters: api.query.serialize(),
+                                       parameters: api.query.serialize() as? [String: Any] ?? [:],
                                     headers: .init(.init(headers())))
             .serializingData()
         
